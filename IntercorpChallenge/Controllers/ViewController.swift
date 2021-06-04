14ae023142dd8c20f4ed054a3620d17c04c17a3d
David Diego Gomez <diegodavid@icloud.com>
2021-06-04 19:52:36 -0300

    First commit. Setup project and App icon

---
+++ IntercorpChallenge/Controllers/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  IntercorpChallenge
+//
+//  Created by David Diego Gomez on 04/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+}
+
